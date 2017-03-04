4707600e636e83e0599105aab203ed7bea8c921c
Saúl Axel <saulaxel.50@gmail.com>
2017-03-03 22:44:05 -0600

    Script to easily run on windows

--- text_based/run_windows.bat
+++ text_based/run_windows.bat
@@ -1,34 +1,49 @@
-echo off
+@echo off
 
-SET TAMANIO_X=140
-SET TAMANIO_Y=70
+:: Tamanio estandar
+::SET TAMANIO_X=80
+::SET TAMANIO_X=23
 
-mode con:cols=%TAMANIO_X% lines=%TAMANIO_Y%
+:: Tamanio personalizado 1
+::SET TAMANIO_X=84
+"SET TAMANIO_Y=43
+
+:: Tamanio personalizado 2
+::SET TAMANIO_X=257
+::SET TAMANIO_Y=110
 
 set argc=0
 for %%x in (%*) do set /a argc+=1 
 
-EJEMPLO[!1!]='ejemplos\circulo.txt'
-EJEMPLO[!2!]='ejemplos\elipse_alta.txt'
-EJEMPLO[!3!]='ejemplos\elipse_acostada.txt'
-EJEMPLO[!4!]='ejemplos\exponencial.txt'
-EJEMPLO[!5!]='ejemplos\logaritmo.txt'
-EJEMPLO[!6!]='ejemplos\parabola.txt'
-EJEMPLO[!7!]='ejemplos\recta.txt'
-EJEMPLO[!8!]='ejemplos\hiperbola_horizontal.txt'
-EJEMPLO[!9!]='ejemplos\hiperbola_vertical.txt'
+set ejemplo1=ejemplos\circulo.txt
+set ejemplo2=ejemplos\elipse_alta.txt
+set ejemplo3=ejemplos\elipse_acostada.txt
+set ejemplo4=ejemplos\exponencial.txt
+set ejemplo5=ejemplos\logaritmo.txt
+set ejemplo6=ejemplos\parabola.txt
+set ejemplo7=ejemplos\recta.txt
+set ejemplo8=ejemplos\hiperbola_horizontal.txt
+set ejemplo9=ejemplos\hiperbola_vertical.txt
 
 if %argc% == 0 goto sin_ejemplo
 if %argc% == 1 goto con_ejemplo
 
 :sin_ejemplo
-    .\graficadora_texto_plano.exe %TAMANIO_X% %TAMANIO_Y%
+
+graficadora_texto_plano.exe %TAMANIO_X% %TAMANIO_Y%
 
 exit /b 0
 
 :con_ejemplo
 
-COMANDO=".\graficadora_texto_plano.exe %TAMANIO_X% %TAMANIO_Y% < !EJEMPLO[%a]}"
+if %1 == 1 graficadora_texto_plano.exe %TAMANIO_X% %TAMANIO_Y% < %ejemplo1%
+if %1 == 2 graficadora_texto_plano.exe %TAMANIO_X% %TAMANIO_Y% < %ejemplo2%
+if %1 == 3 graficadora_texto_plano.exe %TAMANIO_X% %TAMANIO_Y% < %ejemplo3%
+if %1 == 4 graficadora_texto_plano.exe %TAMANIO_X% %TAMANIO_Y% < %ejemplo4%
+if %1 == 5 graficadora_texto_plano.exe %TAMANIO_X% %TAMANIO_Y% < %ejemplo5%
+if %1 == 6 graficadora_texto_plano.exe %TAMANIO_X% %TAMANIO_Y% < %ejemplo6%
+if %1 == 7 graficadora_texto_plano.exe %TAMANIO_X% %TAMANIO_Y% < %ejemplo7%
+if %1 == 8 graficadora_texto_plano.exe %TAMANIO_X% %TAMANIO_Y% < %ejemplo8%
+if %1 == 9 graficadora_texto_plano.exe %TAMANIO_X% %TAMANIO_Y% < %ejemplo9%
 
-%COMANDO%
 exit /b 0
